8c0b1a9f43773e0b24787bd8c3013230423e17f1
planeset94 <planeset94@gmail.com>
2021-05-17 15:57:26 +0200

    JS: linked Vue.JS, add properties to data obj

---
+++ asset/JS/main.js
@@ -0,0 +1,30 @@
+// Descrizione: Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+
+
+const app = new Vue(
+
+    {
+        el: '#root',
+
+        data: {
+
+            giorno: "00",
+            mese: "00",
+            anno: "0000",
+            classColor: "red",
+
+
+
+
+        },
+
+
+
+
+
+    }
+
+
+);
